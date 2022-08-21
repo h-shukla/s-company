647577810cb84c459012f8dc2d56f65ec68b80df
Himanshu Shukla <hshukla2611@gmail.com>
2022-08-21 18:23:03 +0530

    add route to get user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,3 +57,11 @@ exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
         message: 'Logged out'
     })
 })
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id)
+    res.status(200).json({
+        success: true,
+        user
+    })
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { registerUser, loginUser, logoutUser } = require('../controllers/userController')
+const { registerUser, loginUser, logoutUser, getUserDetails } = require('../controllers/userController')
 const router = express.Router()
+const { isAuthenticatedUser, authorizedRoles } = require('../middlewares/auth')
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/logout').get(logoutUser)
+router.route('/me').get(isAuthenticatedUser, getUserDetails)
 
 module.exports = router
