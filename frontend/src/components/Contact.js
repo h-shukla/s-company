c9dc6165270f57181172cd92494b0794bed28bf8
Himanshu Shukla <hshukla2611@gmail.com>
2022-09-08 20:34:24 +0530

    add margin to footer's botton

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -5,8 +5,8 @@ const Contact = () => {
   return (
     <div className="container d-flex flex-column center contact-header">
       <h3>There are several ways to contact us</h3>
-      <p><b>Email:</b>admin@scompany.com</p>
-      <p><b>Phone:</b>0000-0000-00</p>
+      <p><b>Email: </b>admin@scompany.com</p>
+      <p><b>Phone: </b>0000-0000-00</p>
     </div>
   )
 }
