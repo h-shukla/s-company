6d6d5b7b0f7a9577a6dd5fdda7f20d59464a3f5a
Himanshu Shukla <hshukla2611@gmail.com>
2022-09-13 00:19:55 +0530

    refactoring

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -5,6 +5,7 @@ import '../styles/Product.css'
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [search, setSearch] = useState('')
   const [page, setPage] = useState(1)
   const [productsCount, setProductsCount] = useState(0)
   const [loading, setLoading] = useState(true)
@@ -13,11 +14,7 @@ const Products = () => {
   useEffect(() => {
     if (loading) {
       (async () => {
-        const prod = await axios.get(`http://localhost:5000/api/v1/products?page=${page}`, {
-          params: {
-            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMTRhODZlZjg5Y2U5ZTMyMmE2MWFlYiIsImlhdCI6MTY2MjI5ODIyMiwiZXhwIjoxNjYyNzMwMjIyfQ.H7rfYjUnN1G5GUyyH_X5uxDRCM3H8ZAoiKjjBWQw7KA'
-          }
-        })
+        const prod = await axios.get(`http://localhost:5000/api/v1/products?page=${page}`, null)
         setProducts(prod.data.products)
         setProductsCount(prod.data.productsCount)
         setLoading(false)
@@ -40,12 +37,23 @@ const Products = () => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }
 
+  const getFilteredProducts = (products) => {
+    return products.filter(product => product.name.includes(search))
+  }
+
+  const onSearchChange = (e) => {
+    setSearch(e.currentTarget.value)
+  }
+
   return (
     <div className='container d-flex flex-wrap center product-container'>
-      {!loading && <p>{page}/{totalPages} pages</p>}
+      <div className="search">
+        <input type="text" className='form-control' style={{ "minWidth": "30vw" }} value={search} id="searchBar" placeholder='Search products in this page' onChange={onSearchChange} />
+        {!loading && <p>{page}/{totalPages} pages</p>}
+      </div>
       <div className='container d-flex flex-wrap m-2 center'>
         {!loading &&
-          products.map((product) => (
+          getFilteredProducts(products).map((product) => (
             <div className="card m-3" style={{ width: '18rem' }} key={product._id}>
               <img src={product.image} className="card-img-top rounded" alt="" />
               <div className="card-body">
@@ -62,7 +70,6 @@ const Products = () => {
         {page < totalPages && <button type="button" className="btn btn-primary m-1 " onClick={onClickNextPage}>Load more</button>}
         {page > 1 && <button type='button' className='btn btn-secondary m-1' onClick={onPreviousClick}>Previous page</button>}
       </div>
-
     </div>
   )
 }
